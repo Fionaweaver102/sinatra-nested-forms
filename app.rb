5b0d2ebd6b72f9c4fce834b92a7259b9b323ef68
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-12-26 12:09:26 -0800

    commit tree 8b9fe19865a4a9c57adf3a2afdb0f87107966827

--- app.rb
+++ app.rb
@@ -2,8 +2,25 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
 
-  end
-end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship_data|
+        Ship.new(ship_data)
+      end
+
+      @ships = Ship.all
+
+
+      erb :'pirates/show'
+    end
+  end	  
+end	
